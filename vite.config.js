b59acee60700b126a4a7d75ca41506e02a86301b
hafizahpk-1320 <iamgreatcy@gmail.com>
2026-05-13 03:49:52 +0700

    Fix: prioritize .tsx over .jsx in Vite resolve order

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ const __dirname  = path.dirname(__filename)
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.tsx', '.jsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
